bdeb2f5f6243fdce33f3c4e72529eccdd5acdd41
Veepin_Kumar <veepin8459@gmail.com>
2026-03-07 10:17:43 +0530

    MAXLOOT

---
+++ maxLoot.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+using namespace std;
+
+int loot(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    if(n==2) return 2;
+
+    int dp[n+1];
+    dp[0]=0;
+    dp[1]=1;
+    dp[2]=2;
+
+    for(int i=3;i<=n;i++){
+        dp[i]=max(dp[i-1],dp[i-2]+i);
+    }
+    return dp[n];
+}
+int main(){
+    int n=8;
+    
+    cout<<loot(n);
+    return 0;
+
+}
