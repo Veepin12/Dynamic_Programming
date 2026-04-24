925ea869ad22062ad27af26ad0e5411f9c63229c
Veepin_Kumar <veepin8459@gmail.com>
2026-04-24 22:44:34 +0530

    C_vaction

--- Maxprofit.cpp
+++ Maxprofit.cpp
@@ -1,29 +1,62 @@
+//
+//  UniquePath2.cpp
+//  DS_in_Cpp
+//
+//  Created by Veepin Chaudhary on 11/02/26.
+//
+
 #include <iostream>
 #include <vector>
 using namespace std;
-class solution {
-    public:
-int maxloot(vector<int> & nums){
-    int n=nums.size();
-    vector<int> dp;
-    dp.clear();
-    dp.resize(n);
 
-    dp[n-1]=nums[n-1];
-    dp[n-2]=max(nums[n-2],nums[n-1]);
-    
-
-    for(int i=n-3;i>=0;i--){
-        dp[i]+=max(dp[i+2]+nums[i],dp[i+1]);
+class solution{
+public:
+   
+    // Iterative Solution Of the MaxProfit into non Consicutie Array;
+    int fbu(vector<int> & nums){
+        long n=nums.size();
+        vector<int> dp(105);
+        dp.clear();
+        dp.resize(n);
+        dp[n-1]=nums[n-1];
+        dp[n-2]=max(nums[n-1],nums[n-2]);
+        
+        for(int i=n-3;i>=0;i--){
+            dp[i]+=max(nums[i]+dp[i+2] , 0+dp[i+1]);
+            
+        }
+        return dp[0];
+        
+        
     }
-    return dp[0];
-}
-
-
+    
+   //iterative solution
+    int BottumUp(vector<int> & nums){
+         return fbu(nums);
+    }
+    int robber(vector<int> & nums){
+        //Top Down Approhes..
+        
+        long n= nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp(105);
+        dp[0]=nums[0];  // Dp Intialisation using memorization..
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<n;i++){
+            dp[i] += max(dp[i-1], nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
 };
+
+
 int main(){
-    solution S;
-    vector<int> nums={2,7,1,8,5};
-    cout<<S.maxloot(nums)<<endl;
+    
+    class solution S;
+    vector<int> nums={2,7,9,3,4,65,4,14};
+    cout<<S.robber(nums)<<endl;
+    cout<<S.BottumUp(nums)<<endl;
     return 0;
+    
+    
 }
--- vacation.cpp
+++ vacation.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    while(n--){
-        int a1,a2,a3;
-        cin>>a1>>a2>>a3;
 
-        for(int i=0;i<3;i++){
+
+class Solution{
+
+public:
+      
+    int maxpoint(vector<vector<int>> &days){
+        int n = days.size();
+        int m = days[0].size();
+
+        vector<vector<int>> dp( n, vector<int> ( 3,0));
+        
+
+        dp[0][0] =  days[0][0];
+        dp[0][1] =  days[0][1];
+        dp[0][2] =  days[0][2];
+
+        for( int i= 1; i < n; i++){
+            for(int j =0 ; j< 3; j++){
+                dp[i][0] = max({ days[i][0] + max({dp[i-1][1],dp[i-1][2]})});
+                dp[i][1] = max({ days[i][1] + max({ dp[i-1][0],dp[i-1][2]})});
+                dp[i][2] = max({ days[i][2] + max(dp[i-1][0],dp[i-1][1])});
+            }
             
         }
+        return max( {dp[n-1][0],dp[n-1][1],dp[n-1][2]});
     }
+};
+int main(){
+    Solution S;
+    vector<vector<int>> days={{10,20,30},{10,20,30},{30,20,10}};
+    cout<<S.maxpoint(days)<<endl;
+    return 0;
+    
 }
