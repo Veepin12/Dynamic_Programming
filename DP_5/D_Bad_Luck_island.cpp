c77d17ac620276f2775456fd18d60188806cf3ab
Veepin_Kumar <veepin8459@gmail.com>
2026-06-11 19:41:29 +0530

    Rock Paper Scissor

---
+++ DP_5/D_Bad_Luck_island.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double dp[105][105][205];
+
+double rock( int r , int s , int p ){
+        
+    if(r == 0 || s == 0) return 0;
+
+    if(p == 0) return 1;
+   
+   
+    if( dp[r][s][p] > -0.9) return dp[r][s][p];
+    double ans = 0.0;
+    ans += rock(r - 1, s, p) * ( r * p)/( r*s + s* p + p* r);
+    ans += rock(r , s - 1, p) * ( r * s)/( r*s + s* p + p* r);
+    ans += rock(r , s, p - 1) * (  s * p)/( r*s + s* p + p* r);
+    return dp[r][s][p] =  ans;
+    
+
+
+
+}
+double scissior( int r , int s , int p ){
+
+    if(s == 0 || p == 0) return 0;
+    if(r == 0) return 1;
+   
+   
+    if( dp[r][s][p] > -0.9) return dp[r][s][p];
+    double ans = 0;
+    ans += scissior(r - 1, s, p) * ( r * p)/( r*s + s* p + p* r);
+    ans += scissior(r , s - 1, p) * ( r * s)/( r*s + s* p + p* r);
+    ans += scissior(r , s, p - 1) * (  s * p)/( r*s + s* p + p* r);
+    return dp[r][s][p] =  ans;
+    
+    
+
+
+}
+double paper( int r , int s , int p ){
+
+     if(r == 0 || p == 0) return 0;
+   
+    if(s == 0) return 1;
+   
+    if( dp[r][s][p] > -0.9) return dp[r][s][p];
+    double ans = 0;
+    ans += paper(r - 1, s, p) * ( r * p)/( r*s + s* p + p* r);
+    ans += paper(r , s - 1, p) * ( r * s)/( r*s + s* p + p* r);
+    ans += paper(r , s, p - 1) * (  s * p)/( r*s + s* p + p* r);
+    return dp[r][s][p] =  ans;
+    
+    
+
+
+}
+
+int main(){
+    
+
+    int r, s , p;
+    cin>>r>>s>>p;
+
+    
+    memset( dp , - 1.0, sizeof dp);
+
+    cout<<fixed<<setprecision(9)<<rock(r , s , p )<<endl;
+    memset( dp , - 1, sizeof dp);
+
+    cout<<scissior(r, s, p)<<endl;
+    memset( dp , - 1, sizeof dp);
+    cout<<paper(r , s, p)<<endl;
+
+    return 0;
+}
--- DP_5/I_Coins.cpp
+++ DP_5/I_Coins.cpp
@@ -7,7 +7,7 @@ double dp[3005][3005];
 double f( vector<double> & p , int i , int x){
     if(  x == 0) return 1;
     if( i == -1) return 0;
-    if (dp[i][x] > -.9) return dp[i][x];
+    if (dp[i][x] > -0.9) return dp[i][x];
     return dp[i][x] = f( p , i - 1 , x - 1) * p[i] + f(p , i - 1, x)*( 1 - p[i]);
 
     
